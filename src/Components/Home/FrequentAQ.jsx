cb5371cd3135ac288ed03a0fcf99c3a0893e0528
Syeda Umme Ayman <aymanshoity@gmail.com>
2023-11-06 20:36:28 +0600

    Registration with Auth is done and FAQ question havebeen generated

--- src/Components/Home/FrequentAQ.jsx
+++ src/Components/Home/FrequentAQ.jsx
@@ -8,28 +8,28 @@ const FrequentAQ = () => {
             <div className="collapse collapse-arrow bg-gradient-to-b from-black to-[#172554] my-5">
                 <input type="radio" name="my-accordion-2" />
                 <div className="collapse-title text-xl font-medium text-white">
-                    Click to open this one and close others
+                What is Assignment Avengers? What is it for and what can I do here?
                 </div>
                 <div className="collapse-content  text-white">
-                    <p>hello</p>
+                    <p>Assignment Avengers is a study community and a virtual study space that you can access 24/7 from anywhere. When part of our community you will be able to evaluate himself through assignments and share review of the assignments with other students .</p>
                 </div>
             </div>
             <div className="collapse collapse-arrow bg-gradient-to-b from-black to-[#172554] my-5">
                 <input type="radio" name="my-accordion-2" />
                 <div className="collapse-title text-xl font-medium text-white">
-                    Click to open this one and close others
+                    I would like to join. How do I do that?
                 </div>
                 <div className="collapse-content text-white">
-                    <p>hello</p>
+                    <p>Perfect, you are welcome in the community!Please register and login and continue with the assignments</p>
                 </div>
             </div>
             <div className="collapse collapse-arrow bg-gradient-to-b from-black to-[#172554] my-5">
                 <input type="radio" name="my-accordion-2" />
                 <div className="collapse-title text-xl font-medium text-white">
-                    Click to open this one and close others
+                    Are there any regular events or rooms for group discussions, etc?
                 </div>
                 <div className="collapse-content text-white">
-                    <p>hello</p>
+                    <p>Yes! By being part of the community you will have access to a variety of events, from 🧘mindfulness sessions to 📚 study tips bootcamps.</p>
                 </div>
             </div>
         </div>
